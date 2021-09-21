e1e23bf517fe86c60f0f393e40a3fd1c691f31c2
Drian-key <riskidrian1@gmail.com>
2021-09-21 16:39:13 +0700

    file belajar

---
+++ javscript/script.js
@@ -0,0 +1,44 @@
+// // DOM Selection
+// // document.getElementById()
+// const judul = document.getElementById('judul');
+// judul.style.color = 'red';
+// judul.style.backgroundColor = 'yellow';
+// judul.innerHTML = 'Drian p';
+
+// // document.getElementsByTagName() -> HTMLcolection
+// const p = document.getElementsByTagName('p');
+// const alink = document.getElementsByTagName('a');
+// alink[0].style.backgroundColor = 'green';
+// alink[0].innerHTML = "instagram di ubah dari js";
+// for(let i = 0; i < p.length; i++){
+//     p[i].style.backgroundColor = 'red';
+// }
+
+// const h1 = document.getElementsByTagName('h1')[0];
+// h1.style.fontSize = '50px';
+
+// // document.getElementsByClassName()
+// // HTMLcolection
+// const p1 = document.getElementsByClassName('p1')[0];
+// const liitem = document.getElementsByClassName('liitem');
+// p1.innerHTML = 'Ini diubah dari js';
+// for (let i = 0; i < liitem.length; i++) {
+//     liitem[i].style.backgroundColor = '#eaeaea';
+// }
+
+
+
+// // document.querySelector() -> element
+// const p4 = document.querySelector('#b p');
+// const li2 = document.querySelector('#b ul li:nth-child(2)')
+// p4.style.backgroundColor = 'red';
+// p4.style.fontSize = '30px';
+// li2.style.backgroundColor = 'green';
+
+// // document.querySelectorAll()
+// const allP = document.querySelectorAll('p');
+// for (let i = 0; i < allP.length; i++) {
+//     allP[i].style.backgroundColor = 'lightblue';
+// }
+
+
